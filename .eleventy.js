029405478b6c5272c30b4ed2a823998956fcf723
Bhupendra Kumar <bhupendra@digipants.com>
2024-09-23 14:36:25 +0530

    auto

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,12 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/robots.txt");
+  eleventyConfig.addPassthroughCopy("./src/sitemap.xml");
 
-    eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./src/robots.txt');
-    eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
-
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    };
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
